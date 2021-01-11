afc8b62d7faffcfd135d8abed638ccc27a8398d0
Laveena Jethani <laveena.j.21@gmail.com>
2021-01-12 00:53:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading as th
+threads=[]
 count=1
 #udp protocol
 myp=socket.SOCK_DGRAM
@@ -9,21 +10,24 @@ s=socket.socket(afn,myp)
 ip="192.168.43.97"
 port=1234
 s.bind((ip,port))
+def myfun(x):
+    data = x[0].decode()
+    client_IP = x[1][0]
+    client_port = x[1][1]
+    print(client_IP +" : "+data)
+    #sending reply to client
+    reply=input("What reply you want to send to client {}::".format(client_IP))
+    myreply=reply.encode()
+    s.sendto(myreply,(client_IP,client_port))
+
 def server():
     while True:
         x = s.recvfrom(30)
-        data = x[0].decode()
-        client_IP = x[1][0]
-        client_port = x[1][1]
-        print(client_IP +" : "+data)
-        #sending reply to client
-        reply=input("What reply you want to send to client {}::".format(client_IP))
-        myreply=reply.encode()
-        s.sendto(myreply,(client_IP,client_port))
+        myth=th.Thread(target=myfun,args=(x,))
+        threads.append(myth)
+        myth.start()
 
 
-x1=th.Thread(target=server)
-x2=th.Thread(target=server)
 
-x1.start()
-x2.start()
+server()
+
